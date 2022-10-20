09dd5f35c69bd1abec954a029376b32f3ccbe29f
Ryan-M-Burns <ryan.m.burns123@gmail.com>
2022-10-20 03:13:43 +0000

    practice creating INSERT statements

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO users (name, email, password)
+VALUES ('Anouk Van Bogaert', 'canuckWithoutTheC@lighthouselabs.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Taylor Smith', 'MrAnderson@lighthouselabs.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Jaxson Baerg', 'jaxAttack@lighthouselabs.ca', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Posh Apartment', 'Downtown apartment overlooking the chaos around Rogers Arena. A stones throw from footlong $1.50 costco hotdogs!', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',    1500, 1, 1, 1, 'Canada', '1500 Expo Blvd', 'Vancouver', 'British Columbia', 'V1V 1V1', true),
+(2, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 8, 10, 'Canada', '1501 Expo Blvd', 'Vancouver', 'British Columbia', 'V2V 1V1', true),
+(3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 100000, 25, 104, 85, 'Canada', '123 Rich St', 'West Vancouver', 'British Columbia', 'R1R 1R1', true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
